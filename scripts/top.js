7bebab88a4b7fe2912eb069f15fead274be66171
Alp Göncü <79418811+Alpgonjustvi@users.noreply.github.com>
2025-07-30 13:33:55 +0300

    Add files via upload

---
+++ scripts/top.js
@@ -0,0 +1,13 @@
+Array.from(document.querySelector(".top").children).forEach(li => {
+  const headtext = li.innerText
+  const pathname = "/" + headtext.toLowerCase().replace(/\s+/g, "_");
+
+  if (window.location.pathname != pathname) {
+    li.classList.add("other")
+  }
+
+  li.addEventListener("click", () => {
+    window.location.pathname = pathname
+  })
+
+})
